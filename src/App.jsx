623ce17ffd40ee03ecbd78bb79b5ac7cb4359363
wenbo <1501931802@qq.com>
2019-08-30 19:48:06 +0800

    'admin-v1'

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
+import {BrowserRouter,Route,Switch} from "react-router-dom";
 import {Button,message} from "antd";
-   
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'   
 export default class App extends Component {
       handleClick = () => {
           message.success('成功啦...');
       }
   render() {
     return (
-     <Button type='primary' onClick={this.handleClick}>学习</Button>
+      <BrowserRouter>
+          <Switch>
+               <Route path='/login' component={Login}></Route>
+               <Route path='/' component={Admin}></Route>
+          </Switch>
+      </BrowserRouter>
     )
   }
 }
