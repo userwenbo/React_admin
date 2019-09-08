f53d11976443bf922076507dd4079da5c5f68683
wenbo <1501931802@qq.com>
2019-09-08 17:02:12 +0800

    redux vv

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -13,6 +13,7 @@ import User from '../user/user'
 import Bar from '../charts/bar'
 import Line from '../charts/line'
 import Pie from '../charts/pie'
+import NotFound from '../not-found/not-found'
 
 
 const {Footer, Sider, Content } = Layout;
@@ -33,6 +34,7 @@ const {Footer, Sider, Content } = Layout;
           <Header></Header>
           <Content style={{ backgroundColor: 'white', margin: '20px 20px 0' }}>
                 <Switch>
+                    <Redirect from='/' to='home' />  {/* 必须是精确匹配 */}
                     <Route path='/home' component={Home} />
                     <Route path='/category' component={Category} />
                     <Route path='/product' component={Product} />
@@ -41,7 +43,7 @@ const {Footer, Sider, Content } = Layout;
                     <Route path='/charts/bar' component={Bar}/>
                     <Route path='/charts/line' component={Line}/>
                     <Route path='/charts/pie' component={Pie}/>
-                    <Redirect to="/home" />
+                    <Route component={NotFound} />
                 </Switch>
           </Content>
           <Footer style={{textAlign:'center',color:'rgba(0,0,0,0.5)'}}>推荐使用谷歌浏览器，可以获得更佳页面操作体验</Footer>
